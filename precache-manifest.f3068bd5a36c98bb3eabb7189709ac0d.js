6dadcfb25c114aabe59d97a95aac4f1113068669
JamieGrimwood <JamieGrimwood@users.noreply.github.com>
2022-04-04 22:14:30 +0000

    deploy: 49bd798fdbe0e5ef509986aa1777e3e436c5ee57

---
+++ precache-manifest.f3068bd5a36c98bb3eabb7189709ac0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153bc16a1406a6137a663219c2aa8185",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2542e6f5ef2b8bf892c5",
+    "url": "/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "2e07b8a0b3279b311e8e",
+    "url": "/static/css/main.5b0abaed.chunk.css"
+  },
+  {
+    "revision": "2542e6f5ef2b8bf892c5",
+    "url": "/static/js/2.1d736544.chunk.js"
+  },
+  {
+    "revision": "e670feed429893bbdb8c9e1ae51e424c",
+    "url": "/static/js/2.1d736544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e07b8a0b3279b311e8e",
+    "url": "/static/js/main.b3e870f6.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7430bf5ec3c66a828714dd56b9bd9e6c",
+    "url": "/static/media/profile.7430bf5e.jpg"
+  }
+]);
